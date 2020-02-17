66e813bb632b6d18559b6c91c551c6413af76c50
刘敏轩 <1187060103@qq.com>
2020-02-17 21:05:25 +0800

    0217

--- projects/main.py
+++ projects/main.py
@@ -1,8 +1,18 @@
-"""
-test
-"""
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         count_i = 0
+#         while n:
+#             if (n & 1) == 1:
+#                 count_i += 1
+#             else:
+#                 n << 1
+from tkinter import S
 
-import keyword
 
-var = keyword.kwlist
-print(var)
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         return bin(n).count('1')
+#
+#
+# s = Solution()
+# print(s.hammingWeight(6))
