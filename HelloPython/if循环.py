04189054de018d7bc9b7d3bd39dd1867e6515bf4
刘敏轩 <1187060103@qq.com>
2020-02-29 10:17:26 +0800

    0229

---
+++ HelloPython/if循环.py
@@ -0,0 +1,61 @@
+# Created by LiuMinXuan 2020/2/28 18:04 
+# --coding:utf-8--
+
+# if 2 > 1:
+#     print("2")
+# else:
+#     print("1")
+# # 循环
+
+# num = input("输入一个数:")
+# num_1 = input("再输入一个数:")
+#
+# if num_1 > num:
+#     print(num_1)
+# else:
+#     print(num)
+
+# 任何空 都会被当做False处理
+
+# s = " "
+# if s:
+#     print("Null")
+import time
+import sys
+
+# i = 0
+# while i < 10:
+#     time.sleep(1)
+#     print("i的值%d" % i)
+#     print("循环")
+#     i += 1
+
+# str = [1, 2, 3, 4, 5]
+# i = 0
+# for i in str:
+#     print(str[i])
+
+# my_data = ['java', 'c++', 'c', 'python']
+#
+# i = 0
+# while i < len(my_data):
+#     print(my_data[i])
+#     i += 1
+# 遍历字典
+# my_dir = {'java': 90, 'C++': 80, 'python': 70}
+# key_list = list(my_dir.keys())
+# i = 0
+# while i < len(key_list):
+#     print(key_list[i], my_dir[key_list[i]])
+#     i += 1
+# # for in 循环
+# # 遍历字符串
+# str_ = 'qwertyuiop'
+# for c in str_:
+#     print(c)
+
+
+# my_dir = {'java': 90, 'C++': 80, 'python': 70}
+#
+# for c in my_dir.items():
+#     print(c)
---
+++ HelloPython/pyQT.py
@@ -0,0 +1,12 @@
+# Created by LiuMinXuan 2020/2/28 9:26 
+# --coding:utf-8--
+
+import sys
+from PyQt5 import QtWidgets, QtCore
+
+app = QtWidgets.QApplication(sys.argv)
+widget = QtWidgets.QWidget()
+widget.resize(360, 360)
+widget.setWindowTitle("Hello, PyQt5!")
+widget.show()
+sys.exit(app.exec_())
