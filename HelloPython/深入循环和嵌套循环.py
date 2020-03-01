70cb275c10c2f422647b720269ae30fb71fe6bac
刘敏轩 <1187060103@qq.com>
2020-03-01 13:04:06 +0800

    0229

---
+++ HelloPython/深入循环和嵌套循环.py
@@ -0,0 +1,32 @@
+# Created by LiuMinXuan 2020/3/1 12:42 
+# --coding:utf-8--
+# 1. for in 循环遍历列表、字典
+# for i in range(1, 10):
+#     print(i)
+
+# my_list = list(range(0, 10))
+#
+# my_data = {'a': 10, 'b': 20, 'c': 30}
+# for i in my_data.items():
+#     print(i)
+# for i in my_data.keys():
+#     print(i)
+
+# for i in my_data.keys():
+#     print(i)
+# else:
+#     print("end")
+
+# 2.嵌套循环
+# my_data = list(range(1, 100))
+# for i in my_data:
+#     while i > 90:
+#         print(i)
+#         while i > 10:
+#             print(i)
+# 打印乘法口诀表
+# for i in range(1, 10):
+#     for j in range(1, 10):
+#         print("%d*%d=%d" % (i, j, i * j))
+#
+
