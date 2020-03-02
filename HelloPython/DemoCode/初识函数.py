b156bf68adb93a42f468e133c52fe580f950e2a4
刘敏轩 <1187060103@qq.com>
2020-03-02 11:20:47 +0800

    0302

---
+++ HelloPython/DemoCode/初识函数.py
@@ -0,0 +1,23 @@
+# 函数 完成一段功能的代码
+
+# 逻辑上的黑盒子
+
+#  定义函数
+# def func(x, y, z):
+#     print(x, y, z)
+#
+# func(1, 2, 3)
+
+#
+# def first():
+#     print("my first function")
+#
+#
+# first()
+
+# def first():
+#     for i in range(1, 11):
+#         print("i am test")
+#
+# first()
+
--- HelloPython/DemoProjects/面试题.py
+++ HelloPython/DemoProjects/面试题.py
@@ -1,23 +1,22 @@
-SIZE = 6
-array = [[0] * SIZE]
-for i in range(SIZE - 1):
-    array += [[0] * SIZE]
-# 得到二维列表
-# print(array) 1~SIZE*SIZE
-j, k = 0, 0
-orient = 0
-for i in range(1, SIZE * SIZE + 1):
-    array[j][k] = i
-    if orient == 0:
-        j += 1
-    elif orient == 1:
-        k += 1
-    elif orient == 2:
-        k -= 1
-    elif orient == 3:
-        j -= 1
-
-for elem in array:
-    for e in elem:
-        print('%02d' % e, end=' ')
-    print('')
+# SIZE = 6
+# array = [[0] * SIZE]
+# for i in range(SIZE - 1):
+#     array += [[0] * SIZE]
+# # 得到二维列表
+# #print(array) 1~SIZE*SIZE
+# j, k  = 0, 0
+# orient = 0
+# for i in range(1,SIZE):
+#       array[j][k] = i
+#       if orient == 0:
+#               j += 1
+#         elif: orient == 1:
+#
+#
+#
+#
+# for elem in array:
+#     for e in elem:
+#         print('%02d'%e, end = ' ')
+#     print('')
+#
